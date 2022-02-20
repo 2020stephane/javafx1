3526f02d48b89da20c1fdb396cd2d76fc63a6622
2020stephane <69415734+2020stephane@users.noreply.github.com>
2022-02-20 10:25:40 +0100

    test javafx

---
+++ src/application/Main.java
@@ -0,0 +1,33 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Text;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			BorderPane root = new BorderPane();
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setTitle("java fx");
+			Text text = new Text();
+			text.setX(50);
+			text.setY(150);
+			text.setText("Welcome");
+			
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
